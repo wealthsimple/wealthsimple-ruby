d37fc4d1c01be59331129134074c67db652997f5
Peter Graham <peterghm@gmail.com>
2018-02-27 15:01:14 -0500

    remove unneeded attr_reader

--- lib/wealthsimple/errors.rb
+++ lib/wealthsimple/errors.rb
@@ -1,12 +1,11 @@
 module Wealthsimple
   class ApiError < StandardError
-    attr_reader :response
     def initialize(response)
       @response = response
     end
 
     def method_missing(method_name, *args, &block)
-      response.send(method_name, *args, &block)
+      @response.send(method_name, *args, &block)
     end
   end
 end
